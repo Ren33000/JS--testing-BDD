98755ae5cf91c439757ad396d078864d44c7e571
Mylene <pont.mylene@gmail.com>
2021-12-28 20:39:58 +0900

    titleCase function made after the test, working

---
+++ textUtilities.js
@@ -0,0 +1,16 @@
+// taking only expect function from Chai library instead of loading everything
+var expect = require('chai').expect;
+
+function titleCase (title) {
+    var words = title.split(' ');
+    var titleCaseWords = words.map(function (word) {
+        return word[0].toUpperCase() + word.substring(1);
+    });
+    return titleCaseWords.join(' ');
+}
+
+// most difficult test last
+expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
