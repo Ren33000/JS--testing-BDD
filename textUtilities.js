a16457f2774ec049656ada9a3c7186a327be6f3f
Mylene <pont.mylene@gmail.com>
2021-12-28 20:53:07 +0900

    stop before test Suites and specs in mocha

--- textUtilities.js
+++ textUtilities.js
@@ -1,6 +1,7 @@
 // taking only expect function from Chai library instead of loading everything
 var expect = require('chai').expect;
 
+// function made steps by steps with the tests
 function titleCase (title) {
     var words = title.split(' ');
     var titleCaseWords = words.map(function (word) {
@@ -9,7 +10,7 @@ function titleCase (title) {
     return titleCaseWords.join(' ');
 }
 
-// most difficult test last
+// most difficult test to be last
 expect(titleCase('the great mouse detective')).to.be.a('string');
 expect(titleCase('a')).to.equal('A');
 expect(titleCase('vertigo')).to.equal('Vertigo');
